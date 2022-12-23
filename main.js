51eded4b6f01e791d7cef623ee6fcd22d746c7c2
Brayner Mello <braynermll@gmail.com>
2022-12-22 21:13:04 -0300

    monta o html da loja

--- main.js
+++ main.js
@@ -9,9 +9,45 @@ async function getDados(){
 async function montaTela(){
     let dadosSite = await getDados();
 
-    const dados = document.getElementById('dados-loja');
+    const dados = document.getElementById('dados-site');
+
+    const site = dadosSite[0];
     
-    dados.innerHTML = dadosSite[0].dominio;
+    dados.innerHTML = `<h3>informações relevantes sobre o site ${site.dominio}</h3>`;
+
+    dados.innerHTML +=
+     `<table>
+             <tr>
+                <th>Criação do domínio</th>
+                <th>Última alteração de domínio</th>
+                <th>Vencimento</th>
+                <th>Data - Situação</th>
+                <th>CNPJ</th>
+                <th>CNPJ ativo?</th>
+                <th>Telefone</th>
+                <th>Email</th>
+                <th>Endereço</th>
+            </tr>
+             <tr>
+                <td>${site.criacao_dominio}</td>
+                <td>${site.ultima_alteracao}</td>
+                <td>${site.vencimento}</td>
+                <td>${site.data_situacao}</td>
+                <td>${site.cnpj}</td>
+                <td>${site.cnpj_ativo? 'Sim' : 'Não'}</td>
+                <td>${site.telefone}</td>
+                <td>${site.email}</td>
+                <td>
+                    ${site.endereco.logradouro},
+                    ${site.endereco.numero},
+                    ${site.endereco.cep},
+                    ${site.endereco.municipio}, 
+                    ${site.endereco.uf}
+                </td>
+             </tr>
+     </table>`
+
+
     
 }
 montaTela();
